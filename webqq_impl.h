71b4b11b0830eb66da69b4f5c825f4fd219d1ae0
microcai <microcaicai@gmail.com>
2013-04-05 17:46:45 +0800

    fix "加载哪个群? 你没设置啊!"

--- webqq_impl.h
+++ webqq_impl.h
@@ -181,6 +181,7 @@ private:
 	void cb_search_group(std::string, const boost::system::error_code& ec, read_streamptr stream,  boost::asio::streambuf & buf, webqq::search_group_handler handler);
 	
 	void fetch_aid(std::string adi, boost::uint64_t _time_, boost::function<void(const boost::system::error_code&, std::string)> handler);
+
 public:
 	std::string	m_vfwebqq;
 	LwqqCookies m_cookies;
