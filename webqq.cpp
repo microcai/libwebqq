4d89083e2ca6423d3eba991490765855de4e1be8
kiki <kiki.caicai@gmail.com>
2013-07-07 17:49:51 +0800

    update copyright to 2013

--- webqq.cpp
+++ webqq.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012  微蔡 <microcai@fedoraproject.org>
+ * Copyright (C) 2012 - 2013  微蔡 <microcai@fedoraproject.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
