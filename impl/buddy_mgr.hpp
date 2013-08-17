8064ca171c88696a2cf36aa316609999abc0edcb
microcai <microcaicai@gmail.com>
2013-08-17 21:26:47 +0800

    move buddy mgr to seperated lib

--- impl/buddy_mgr.hpp
+++ impl/buddy_mgr.hpp
@@ -32,36 +32,7 @@ public:
 		db_initialize();
 	}
 
-	void update_group_list(std::string gid, std::string name, std::string code)
-	{
-		using namespace soci;
-
-		std::string qqnum, owner;
-
-		soci::indicator qqnum_indicator, owner_indicator;
-
-		transaction trans(m_sql);
-
-		m_sql << "select qqnum, owner from groups where group_code= :group_code"
-			, into(qqnum, qqnum_indicator), into(owner, owner_indicator), use(code);
-
-		if (qqnum_indicator == i_null)
-			qqnum = "";
-
-		if (owner_indicator == i_null)
-			owner = "";
-
-		m_sql << "delete from groups where gid = :gid or group_code = :group_code "
-			, use(gid), use(code);
-
-		m_sql << "insert into groups "
-			"(gid, group_code, name, qqnum, owner, generate_time)"
-				" values "
-			"(:gid, :group_code, :name, :qqnum, :owner, datetime('now') )"
-			, use(gid), use(code), use(name), use(qqnum), use(owner);
-
-		trans.commit();
-	}
+	void update_group_list(std::string gid, std::string name, std::string code);
 
 private:
 
