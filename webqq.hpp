fe7a0194aae2eaf7d1aa9f8dda0221b9ec6877d4
microcai <microcaicai@gmail.com>
2013-09-04 14:13:34 +0800

    BOOST_SYMBOL_VISIBLE 来控制。
    将来可能build为 DLL 呢！另外默认使用 visibility=hidden 将减少符号表的大小。

--- webqq.hpp
+++ webqq.hpp
@@ -22,6 +22,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <boost/config.hpp>
 #include <boost/function.hpp>
 #include <boost/asio.hpp>
 #include <boost/function.hpp>
@@ -29,35 +30,10 @@
 #include <boost/signals2.hpp>
 #include <boost/any.hpp>
 
-#if defined _WIN32 || defined __CYGWIN__
-#ifdef BUILDING_DLL
-#ifdef __GNUC__
-#define DLL_PUBLIC __attribute__ ((dllexport))
-#else
-#define DLL_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
-#endif
-#else
-#ifdef __GNUC__
-#define DLL_PUBLIC __attribute__ ((dllimport))
-#else
-#define DLL_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
-#endif
-#endif
-#define DLL_LOCAL
-#else
-#if __GNUC__ >= 4
-#define DLL_PUBLIC __attribute__ ((visibility ("default")))
-#define DLL_LOCAL  __attribute__ ((visibility ("hidden")))
-#else
-#define DLL_PUBLIC
-#define DLL_LOCAL
-#endif
-#endif
-
-namespace webqq{
+namespace webqq {
 namespace qqimpl {
 class WebQQ;
-};
+} // namespace qqimpl
 
 typedef enum LWQQ_STATUS {
 	LWQQ_STATUS_UNKNOW = 0,
@@ -144,7 +120,7 @@ struct qqMsg {
 	std::string	cface_data;
 };
 
-class webqq {
+class BOOST_SYMBOL_VISIBLE webqq {
 public:
 	typedef boost::function<void(boost::system::error_code)> webqq_handler_t;
 	typedef boost::function<void(boost::system::error_code, std::string )> webqq_handler_string_t;
